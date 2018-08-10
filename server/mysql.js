6131d0ee723bf2879caed96b81ffa7a4456734d2
Elon <elonybear@gmail.com>
2018-08-09 23:35:47 -0400

    postgres client

--- server/mysql.js
+++ server/mysql.js
@@ -1,9 +1,10 @@
 import mysql from 'mysql';
 var config = {
-  host     : 'localhost',
-  user     : 'root',
-  password : 'root',
-  database: 'social_attitudes'
+  host     : process.env.DB_HOST,
+  port     : process.env.DB_PORT,
+  user     : process.env.DB_USER,
+  password : process.env.DB_PW,
+  database : process.env.DB
 };
 
 var connection = mysql.createConnection( config );
